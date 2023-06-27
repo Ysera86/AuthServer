389ab43030900d7660d51a1b1b7ab0f8545729cd
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-27 21:08:36 +0300

    Implement ErrorDto for all projects to use

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; }
+        public bool IsShown { get; private set; } // set false for developer
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShown)
+        {
+            Errors.Add(error);
+            IsShown = isShown;
+        }
+
+        public ErrorDto(List<string> errors, bool isShown)
+        {
+            Errors = errors;
+            IsShown = isShown;
+        }
+    }
+}
