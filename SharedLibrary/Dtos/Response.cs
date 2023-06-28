53ef22ed7d8cd39578fa8aca6304af9695feb159
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-28 08:43:52 +0300

    Implement Response class for all projects to use

---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+        public ErrorDto Error { get; private set; }
+        /// <summary>
+        /// For internal use
+        /// </summary>
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        }
+        public static Response<T> Fail(ErrorDto error, int statusCode)
+        {
+            return new Response<T> { StatusCode = statusCode, Error = error, IsSuccessful = false };
+        }
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShown)
+        {
+            return new Response<T> { StatusCode = statusCode, Error = new ErrorDto(errorMessage, isShown), IsSuccessful = false };
+        }
+    }
+}
