d73d72cfede5c2d28d9d670103990df4cb99b54c
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-29 13:03:05 +0300

    Implement mapping with Automapper lib. Create ObjectMapper with lazy loading

---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        // ObjectMapper.Mapper diye çağırana kadar içerideki kod çalışıp da memorye yüklenmicek > Lazy ile Lazy Loading yaptık.
+        // örn bi classın bir propertysinde çok nüyük data olabilir. ihtiyaç duymadıkça yüklenmesin isteriz. o zaman lazy loading yaparız.
+
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
