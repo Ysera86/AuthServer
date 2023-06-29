54a1e355f8517056834e8bb69bbe494666b36802
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-29 22:14:22 +0300

    Add dependencies into Startup.cs

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -1,8 +1,18 @@
 using AuthServer.Core.Configuration;
+using AuthServer.Core.GenericServices;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data;
+using AuthServer.Data.Repositories;
+using AuthServer.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,11 +38,42 @@ namespace AuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //..
+
+            // DI Register
+            // AddSingleton : Uygulama boyunca tek 1 instance
+            // AddScoped : Ýstek baþýna 1 instance, kaç kere ayný interfacei istersek isteyelim
+            // AddTransient : Ayný interface ile her karþýlaþtýðýnda yeni 1 instance
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly("AuthServer.Data");
+                });
+            });
+
+            services.AddIdentity<UserApp, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireNonAlphanumeric = false; // NonAlphanumeric > *, ? , = etc.
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
 
             // Options Pattern
             services.Configure<CustomTokenOptions>(options => Configuration.GetSection("TokenOption"));
             services.Configure<List<Client>>(Configuration.GetSection("Clients"));
 
+
+            //..
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
