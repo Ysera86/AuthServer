9432cded4517e9c24d851a7054b5f376c411cb34
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-29 09:33:06 +0300

    Create ITokenService

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        /// <summary>
+        /// Bu clientın erişebileceği apiler.
+        /// www.myapi1.com, www.myapi2.com etc.
+        /// </summary>
+        public List<string> Audiences { get; set; }
+    }
+}
