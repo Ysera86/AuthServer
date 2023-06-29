4321df1d0006227fad8ac02a34f13265c4201122
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-29 09:42:33 +0300

    Create IAuthenticationService

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using AuthServer.Core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto); // ClientLoginDto 5e kdr appsettingste tutabilirim çok olmadığından
+    }
+}
