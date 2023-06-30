93cdc8778c785bc0468f0f080fb35d00326c7070
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-06-30 14:19:42 +0300

    Implement ProductController, simple CRUD endpoints

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.GenericServices;
+using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IGenericService<Product, ProductDto> _productService;
+
+        public ProductController(IGenericService<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]   
+        public async Task<IActionResult> GetAll()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Remove(productDto.Id));
+        }
+    }
+}
